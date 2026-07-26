1b662b964773c1512ce9d25f93f9eed6fc783bd5
rezt <118548695+rezt2023@users.noreply.github.com>
2026-07-25 21:07:06 -0300

    Update index.js

--- index.js
+++ index.js
@@ -40,16 +40,6 @@ const PAID_CAPES_WHITELIST = {
         "7fc49989-b62b-4877-833a-19ced916cf43",
         "a1541f0e-a467-403b-bdf2-759cb33647bf",
         "391d0bc2-1210-402e-88d4-063a1d30dc7c"
-    ],
-    "Sakura.gif": [
-        "7fc49989-b62b-4877-833a-19ced916cf43",
-        "a1541f0e-a467-403b-bdf2-759cb33647bf",
-        "391d0bc2-1210-402e-88d4-063a1d30dc7c"
-    ],
-    "CatGirl.gif": [
-        "7fc49989-b62b-4877-833a-19ced916cf43",
-        "a1541f0e-a467-403b-bdf2-759cb33647bf",
-        "391d0bc2-1210-402e-88d4-063a1d30dc7c"
     ]
 };
 
