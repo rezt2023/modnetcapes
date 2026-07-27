669cf47aa52e414d34a898614dd3ec3301be851c
rezt <118548695+rezt2023@users.noreply.github.com>
2026-07-26 23:31:27 -0300

    Update index.js

--- index.js
+++ index.js
@@ -51,8 +51,7 @@ const ALLOWED_CAPES = new Set([
     "Moonlight_Trail", "MrMessiah", "New_Years", "Oxeye", "Pan", 
     "Prismarine", "Purple_Heart", "Realms_Map_Maker", "Scrolls_Champion", 
     "Translator", "Turtle", "Valentine", "Vanilla", 
-    "Yearn", "Zombie_Horse", "Developer", "YouTuber",
-    "Donator", "Acreano", "Brasil"
+    "Yearn", "Zombie_Horse", "Brasil"
 ]);
 
 const PAID_CAPES_WHITELIST = {
